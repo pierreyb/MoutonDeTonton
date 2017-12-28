94c6a5486b1782adeacb84bad69bf836a3fe0301
Pierre-Yves Bernard <pierreyvesbernard@yahoo.com>
2017-12-28 12:42:22 +0100

    Refactoring menu

--- MoutonDeTonton/urls.py
+++ MoutonDeTonton/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
@@ -29,4 +29,5 @@ urlpatterns += [
 
 #Add static pages
 urlpatterns += [
+    url(r'$', TemplateView.as_view(template_name='base.html'),)
 ]
